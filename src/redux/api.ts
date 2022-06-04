e341f80743547494248dcb4788074b1d720ddae5
Prashant <74547535+Prashant-student@users.noreply.github.com>
2022-06-04 20:54:53 +0530

    last

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,6 +1,7 @@
 // export const base = "https://sih-backend-try.herokuapp.com"
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
-export const base = "http://localhost:5000"
+// export const base = "http://localhost:5000"
+export const base = "http://back.eba-gmpkm8tq.ap-south-1.elasticbeanstalk.com/"
 
 export const apiadd = `${base}`
 
